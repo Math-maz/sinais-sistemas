a3bf4adaaaeb7d166f279839ca5f0b40ca329fe1
Matheus Pereira Araujo <matheuspereiraaraujo@MacBook-Pro-de-Regina.local>
2019-10-06 17:06:32 -0300

    Mudar nomenclatura da 1.42 e adicionar plots na 1.46

--- sinais_sistemas/questao_1.44.py
+++ sinais_sistemas/questao_1.44.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 t = np.arange(-0.002, 0.002, 0.0001)
-for a in range(500, 1250, 250):
+a_values = [500, 750, 1000]
+for a in a_values:
     fx = 20*np.sin(2*np.pi*1000*t - np.pi/3) * np.exp(-a*t)
     plt.plot(t, fx)
     plt.show()
--- sinais_sistemas/questao_1.45.py
+++ sinais_sistemas/questao_1.45.py
@@ -5,5 +5,5 @@ F = 0.1
 n = np.arange(-0.5*F, 0.5*F, 0.001)
 wn = np.cos(2*np.pi * F * n)
 plt.plot(n, wn)
-#plt.ylim(-, 6)
+# plt.ylim(-, 6)
 plt.show()
