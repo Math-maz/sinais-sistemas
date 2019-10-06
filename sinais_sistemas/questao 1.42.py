1ce7cb562d2f1481f6983005e5e4b357f2a3f619
Jean-mrs <jeanmoraes1994@hotmail.com>
2019-10-06 16:14:05 -0300

    adicao da ultima questao

--- sinais_sistemas/questao 1.42.py
+++ sinais_sistemas/questao 1.42.py
@@ -1,45 +1,29 @@
 from scipy import signal
 import numpy as np
-
-
 import matplotlib.pyplot as plt
-t = np.linspace(0, 6, 1257, endpoint=False)
 
-
-a = np.arange (-2*np.pi, 2*np.pi, 0.01)
-b = 2*np.sin(a)
-for n in range(2, 2002, 1):
-     b = b - 2.*((-1.)**n/n)*np.sin(n*a)
-plt.plot(a, b, 'b-')
-plt.show()
-
+t = np.linspace(0, 8, 500)
 amplitude = 5
-plt.plot(t, amplitude * signal.square(2 * np.pi * 20 * t, duty=(b+1)/2))
-plt.ylim(-6, 6)
+frequencia = 20
+ciclo_trabalho = 0.6
+
+for ciclo in range(1, 6):
+     onda_quadrada = amplitude * signal.square(2 * np.pi * frequencia * t, duty=ciclo/10)
+     dente_serra = amplitude * signal.sawtooth(2 * np.pi * frequencia * t, width=ciclo/10)
+     fig, axs = plt.subplots(2)
+     fig.suptitle('Aproximação de Ondas Periódicas de Tempo Contínuo com Ciclo: ' + str(ciclo/10))
+     axs[0].plot(t, onda_quadrada, color='red')
+     axs[1].plot(t, dente_serra, color='green')
 plt.show()
 
 
-plt.figure()
-sig = np.sin(2 * np.pi * t)
-pwm = signal.square(2 * np.pi * 30 * t, duty=(sig + 1)/2)
-plt.subplot(2, 1, 1)
-plt.plot(t, sig)
-plt.subplot(2, 1, 2)
-plt.plot(t, pwm)
-plt.ylim(-1.5, 1.5)
-
-plt.show()
-
-
-
-a = np.arange (-2*np.pi, 2*np.pi, 0.01)
-b = 2*np.sin(a)
-for n in range (2,1002,1):
-     b = b - 2.*((-1.)**n/n)*np.sin(n*a)
-
-plt.plot (a,b, 'b-')
-plt.plot(a, 5*signal.square(2 * np.pi * 5 * a))
-plt.ylabel ('f(x)')
-plt.grid()
-plt.title ('(a)')
-plt.show()
+# plt.figure()
+# sig = np.sin(2 * np.pi * t)
+# pwm = signal.square(2 * np.pi * 30 * t, duty=(sig + 1)/2)
+# plt.subplot(2, 1, 1)
+# plt.plot(t, sig)
+# plt.subplot(2, 1, 2)
+# plt.plot(t, pwm)
+# plt.ylim(-1.5, 1.5)
+#
+# plt.show()
